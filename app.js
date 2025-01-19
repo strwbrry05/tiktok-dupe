67168cca6a0f5c04dde1cefeed8e99079f4d750f
strwbrry05 <jq.strwbrry05@gmail.com>
2025-01-18 22:23:58 -0800

    testing buttons

---
+++ app.js
@@ -0,0 +1,25 @@
+const goForward = document.querySelector('.goForward');
+const goBack = document.querySelector('.goBack');
+const videoPlayback = document.querySelector('.videoPlayback');
+
+goForward.addEventListener('click', nextVideo);
+goBack.addEventListener('click', previousVideo);
+
+let videoNum = 1;
+
+function nextVideo() {
+    videoNum++;
+    if (videoNum === 4) {
+        videoNum = 1;
+    }
+    videoPlayback.innerHTML = `<video width="320px" src="media/${videoNum}.mp4" autoplay></video>`
+}
+
+function previousVideo() {
+    videoNum--;
+    if (videoNum === 0) {
+        videoNum = 3;
+    }
+    videoPlayback.innerHTML = `<video width="320px" src="media/${videoNum}.mp4" autoplay></video>`
+
+}
